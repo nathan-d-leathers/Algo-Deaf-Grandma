0df0c3c9ab0dd00438807fb02b04add9c646d905
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-24 15:00:07 -0700

    i think i finally figured this out

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,19 @@
 function deafGrandma() {
+    let regex = /[a-z]/g;
+    let goodbyeCount = 0;
+    if (str == "GOODBYE" && goodbyeCount == 0) {
+      console.log("LEAVING SO SOON?");
+      goodbyeCount++;
+    } else if (str == "GOODBYE" && goodbyeCount == 1) {
+      return "LATER, SKATER!";
+    } else if (str.length == 0) {
+      console.log("WHAT?!");
+    } else if (regex.test(str) == true) {
+      console.log("SPEAK UP KID!");
+    } else if (regex.test(str) == false) {
+      console.log("NO, NOT SINCE 1946!");
+    } 
+  };
 
-}
 
 deafGrandma();
