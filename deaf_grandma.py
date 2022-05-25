cea0e2c47ec4e5a7cf16527ccfb6970f0e19aac4
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-25 12:52:43 -0700

    solved minus one param

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,24 @@
-def deaf_grandma():
-
-
-
-deaf_grandma()
+def deaf_grandma(str):
+    response = ""
+    byCount = 0
+    
+    if len(str) == 0:
+        return("WHAT!?")   
+    if str.isupper() == True:
+        response = "NO, NOT SINCE 1946!"
+    else:
+        response = "SPEAK UP KID!"
+    if str == "GOODBYE" and byCount > 0:
+        return("LATER, SKATER!")
+    if str == "GOODBYE" and byCount == 0:
+        byCount += 1
+        response = "LEAVING SO SOON?"
+    return(response)
+    
+# Solved except for final GOODBYE, struggling to keep count of byCount when returning reponses
+    
+print(deaf_grandma("hello"))
+print(deaf_grandma("HELLO"))
+print(deaf_grandma(""))
+print(deaf_grandma("GOODBYE"))
+print(deaf_grandma("GOODBYE"))
