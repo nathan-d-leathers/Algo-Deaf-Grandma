f00d4be7748880c6d09c4ee62579442f36ba3958
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-04 17:46:12 -0700

    solved w/ inputs in js and py

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,21 +1,23 @@
 function deafGrandma() {
     let regex = /[a-z]/g;
     let goodbyeCount = 0;
+    let str = window.prompt("Hey Kid! ")
+
+    while (goodbyeCount < 2) {
     if (str == "GOODBYE" && goodbyeCount == 0) {
-      console.log("LEAVING SO SOON?");
+      str = window.prompt("LEAVING SO SOON?");
       goodbyeCount++;
     } else if (str == "GOODBYE" && goodbyeCount == 1) {
-      return "LATER, SKATER!";
+      str = alert("LATER, SKATER!");
     } else if (str.length == 0) {
-      console.log("WHAT?!");
+      str = window.prompt("WHAT?!");
     } else if (regex.test(str) == true) {
-      console.log("SPEAK UP KID!");
+      str = window.prompt("SPEAK UP KID!");
     } else if (regex.test(str) == false) {
-      console.log("NO, NOT SINCE 1946!");
+      str = window.prompt("NO, NOT SINCE 1946!");
     } 
+    }
   };
 
 
-deafGrandma();
-
-// neeed window.prompt to sucessfuly use user inputs
+console.log(deafGrandma());
--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,18 +1,29 @@
 def deaf_grandma():
-    byCount = 0
-    kid_rep = input()
-    if len(kid_rep) == 0:
-        print("WHAT!?")   
-    if kid_rep.isupper() == True:
-        print("NO, NOT SINCE 1946!")
-    else:
-        print("SPEAK UP KID!")
-    if kid_rep == "GOODBYE" and byCount > 0:
-        print("LATER, SKATER!")
-    if kid_rep == "GOODBYE" and byCount == 0:
-        byCount += 1
-        print("LEAVING SO SOON?")
+    bye_count = 0
+    kid_rep = input("HEY KID! ")
+    
+    while bye_count < 2:
+        if len(kid_rep) == 0:
+            kid_rep = input("WHAT? ")  
+        elif kid_rep == "GOODBYE" and bye_count > 0:
+            return("LATER, SKATER! ")
+        elif kid_rep == "GOODBYE" and bye_count == 0:
+            bye_count += 1
+            kid_rep = input("LEAVING SO SOON? ")
+        elif kid_rep.isupper() == True:
+            kid_rep = input("NO, NOT SINCE 1946! ")
+        else:
+            kid_rep = input("SPEAK UP KID! ")
+
+ 
  
 print(deaf_grandma())
 
-# need to use user inputs to solve problem without exiting code function
+# output in console:
+
+# HEY KID! hi grandma
+# SPEAK UP KID! HELLO
+# NO, NOT SINCE 1946!  
+# WHAT? GOODBYE
+# LEAVING SO SOON? GOODBYE
+# LATER, SKATER! 
